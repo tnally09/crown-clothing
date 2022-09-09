219d74d33cbbec326ea7225629476efd81a381b0
tylernally <tylernally@microsoft.com>
2022-09-09 11:43:31 -0500

    created checkout page and components

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,32 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
+import './checkout-item.styles.scss';
+
+const CheckoutItem = ({cartItem}) => {
+    const { addItemToCart, removeItemFromCart, clearItemFromCart } = useContext(CartContext);
+    const { imageUrl, name, quantity, price } = cartItem
+
+    const decrementItem = () => removeItemFromCart(cartItem);
+    const incrementItem = () => addItemToCart(cartItem);
+    const clearItemHandler = () => clearItemFromCart(cartItem);
+    
+    return(
+        <div className='checkout-item-container'>
+            <div className='image-container'>
+                <img src={imageUrl} alt={`${name}`} />
+            </div>
+            <span className='name'> {name} </span>
+            <span className='quantity'> 
+                <div className='arrow' onClick={decrementItem}>&#10094;</div>
+                <span className='value'> {quantity} </span>
+                <div className='arrow' onClick={incrementItem}>&#10095;</div>
+            </span>
+            <span className='price'> {price} </span>
+            <div className='remove-button' onClick={clearItemHandler}>&#10005;</div>
+        </div>
+    );
+};
+
+export default CheckoutItem;
