59d84d8e340648a9d9495706f15e8b02d52cf917
tylernally <tylernally@microsoft.com>
2022-09-21 08:50:44 -0500

    remove unused method, update firebase location

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState, useEffect } from "react";
 
-import { addCollectionAndDocuments, getCategoriesAndDocuments } from "../utils/firebase.utils";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 
 // import SHOP_DATA from "../shop-data";
 
