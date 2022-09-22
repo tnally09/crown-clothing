2049f209d3265a7335c2b0ec22ce8ab670dde998
tylernally <tylernally@microsoft.com>
2022-09-22 08:40:59 -0500

    install redux, add root, user, category reducers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 import { Routes, Route } from 'react-router-dom';
 
+import { createUserDocumentFromAuth, onAuthStateChangedListener } from "./utils/firebase/firebase.utils";
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
+import { setCurrentUser } from './store/user/user.action';
 
 import './categories.styles.scss'
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+        if (user) {
+          createUserDocumentFromAuth(user);
+        }
+        dispatch(setCurrentUser(user));
+      });
+      
+      return unsubscribe;
+  }, []); // Not a true lint error, because dispatch does not change (can pass it if desired, but unnecessary)
+  
+  console.log('app')
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,12 @@
 import { Fragment, useContext } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
-import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
+import { selectCurrentUser } from '../../store/user/user.selector';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
@@ -13,8 +14,9 @@ import { signOutUser } from '../../utils/firebase/firebase.utils';
 import { NavigationContainer, LogoContainer, NavLinks, NavLink } from './navigation.styles';
 
 
+
 const Navigation = () => {
-    const { currentUser } = useContext(UserContext);
+    const currentUser = useSelector(selectCurrentUser);
     const { isCartOpen } = useContext(CartContext);
 
     
