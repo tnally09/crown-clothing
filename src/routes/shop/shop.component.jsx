9bcac71a1bb17bf225d6e74aac4de72ca6944917
tylernally <tylernally@microsoft.com>
2022-10-07 10:36:35 -0500

    implementing thunk

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,22 +4,13 @@ import { useDispatch } from 'react-redux';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategories } from '../../store/categories/category.action';
+import { fetchCategoriesAsync } from '../../store/categories/category.action';
 
 const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        const getCategoriesMap = async () => {
-            try {
-                const categories = await getCategoriesAndDocuments('categories');
-                dispatch(setCategories(categories));
-            } catch(error) {
-                console.log(error);
-            }
-        }
-        getCategoriesMap();
+        dispatch(fetchCategoriesAsync());
     }, [])
 
     return (
