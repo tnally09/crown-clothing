3023837fa873b452a2743bc1ee9770b6e10561e2
tylernally <tylernally@microsoft.com>
2022-10-07 08:26:56 -0500

    adding stripe functionality

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { selectCartItems, selectCartTotal } from "../../store/cart/cart.selector";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import PaymentForm from "../../components/payment-form/payment-form.component";
 
 import { CheckoutContainer, CheckoutHeader, HeaderBlock, Total } from "./checkout.styles";
 
@@ -31,6 +32,7 @@ const Checkout = () => {
             </CheckoutHeader>
             {cartItems.map((cartItem) => <CheckoutItem key={cartItem.id} cartItem={cartItem} />)}
             <Total>Total: ${cartPriceTotal} </Total>
+            <PaymentForm />
         </CheckoutContainer>
     );
 };
